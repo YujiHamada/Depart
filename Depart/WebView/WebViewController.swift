80eea4eb8495d411f97d6f617c913d14134e01c0
濱田裕史 <uziu2hahaha@gmail.com>
2019-06-15 21:13:53 +0900

    いったんコミット

--- Depart/WebView/WebViewController.swift
+++ Depart/WebView/WebViewController.swift
@@ -23,16 +23,33 @@ class WebViewController: UIViewController {
         
         let flexible = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.flexibleSpace, target: self, action: nil)
         let shareButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.action, target: self, action: #selector(self.share))
-        shareButton.imageInsets = UIEdgeInsets(top: 0.0, left: 0.0, bottom: 0, right: 30)
+        shareButton.imageInsets = UIEdgeInsets(top: 0.0, left: 0.0, bottom: 0, right: 0)
         let closeButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.stop, target: self, action: #selector(self.close))
-        closeButton.imageInsets = UIEdgeInsets(top: 4.0, left: 0.0, bottom: 0, right: 15)
+        closeButton.imageInsets = UIEdgeInsets(top: 4.0, left: 0.0, bottom: 0, right: 0)
         
-        bottomToolbar.items = [flexible, shareButton, closeButton]
+//        let screenshotButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.stop, target: self, action: #selector(self.screenshot))
+        let screenshotButton = UIBarButtonItem(title: "記事をスクショする！", style: .plain, target: self, action: #selector(screenshot))
+        
+
+        closeButton.imageInsets = UIEdgeInsets(top: 4.0, left: 0.0, bottom: 0, right: 0)
+        
+        bottomToolbar.items = [flexible, screenshotButton, flexible, shareButton, flexible, closeButton]
         
         if let safeAreaInsetsTop = UIApplication.shared.delegate?.window??.safeAreaInsets.top {
             bottomToolbar.frame = CGRect(x: bottomToolbar.frame.origin.x, y: bottomToolbar.frame.origin.y, width: bottomToolbar.frame.size.width, height: bottomToolbar.frame.size.height +  safeAreaInsetsTop)
         }
 
+    }
+    
+    @IBAction func screenshot() {
+        let webViewFrame = webView.frame
+        
+        webView.frame = CGRect(x: webViewFrame.origin.x, y: webViewFrame.origin.y, width: webView.scrollView.contentSize.width, height: webView.scrollView.contentSize.height)
+        
+        UIGraphicsBeginImageContextWithOptions(webView.scrollView.contentSize, false, 0);
+        self.webView.layer.render(in: UIGraphicsGetCurrentContext()!)
+        let image:UIImage = UIGraphicsGetImageFromCurrentImageContext()!;
+        UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
     }
     
     @objc func close() {
